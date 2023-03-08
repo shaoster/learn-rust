53f01a404276eeed43fabd33c4ea31956d5a3d02
shaoster <shaoster@gmail.com>
2023-03-08 08:42:41 -0500

    - Experiment with passing mutable references down the stack.   Everybody has to know it's mutable all the way up.   That's super reasonable! - Get rid of the launch.json. Using the project launch settings in   vscode, instead of those provided by extensions, will crash vscode   when a non-rust file is in focus.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+/// How does the mutable buffer reference ride the stack?
+fn get_line_from_stdin_and_eprint_bytes_read(buf: &mut String) -> () {
+    let bytes_read = io::stdin()
+        // buf is already a mutable reference. We should not (and cannot) re-borrow it.
+        .read_line(buf)
+        .expect("Failed to read line");
+    eprintln!("Read {bytes_read} bytes.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let mut guess = String::new();
+    /*
+    No use after borrow, so the &mut prefix must be supplied in this layer.
+    "References are immutable by default. Hence, you need to write &mut guess rather than &guess".
+    */
+    get_line_from_stdin_and_eprint_bytes_read(&mut guess);
+    println!("You guessed: {guess}");
 }
