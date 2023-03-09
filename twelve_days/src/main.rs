6960c84b01753fdd41501acea1c5bdb498d9a558
shaoster <shaoster@gmail.com>
2023-03-08 19:49:45 -0500

    Twelve Days of Thing(s) Doing Things

--- twelve_days/src/main.rs
+++ twelve_days/src/main.rs
@@ -1,3 +1,26 @@
+const ORDINALS: [&str; 12] = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+];
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..12 {
+        let ordinal = ORDINALS[i];
+        println!("On the {ordinal} day of Christmas, my true love gave to me");
+        for j in 0..=i {
+            let day_number = i - j + 1;
+            // Abstract base lyrics.
+            println!("{day_number} thing(s) doing things");
+        }
+    }
 }
