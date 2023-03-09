28d21f80b0e7eeb4a260867bebe37c21213e3066
shaoster <shaoster@gmail.com>
2023-03-08 19:36:44 -0500

    Temperature I guess

--- temperature/src/main.rs
+++ temperature/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+/// Convert f to c, I guess?
 fn main() {
-    println!("Hello, world!");
+    for line in io::stdin().lines() {
+        let f: f64 = match line {
+            Ok(line) => match line.trim().parse() {
+                Ok(f) => f,
+                Err(_) => continue,
+            },
+            Err(_) => continue,
+        };
+        let c = (f - 32.0) / 1.8;
+        println!("{f} is {c} in Celsius");
+    }
 }
